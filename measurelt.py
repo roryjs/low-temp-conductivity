2129e01b0f508a3afcfc758aac544d320d00f391
Rory J Sanderson <rory@rjsanderson.com>
2017-02-14 13:12:12 +0000

    Major refactor. Allow temperature ramp.
    Create multiple functions. Use constant current and voltage for now.
    Temperature ramp can be set with arguments.

--- measurelt.py
+++ measurelt.py
@@ -19,77 +19,99 @@ from time import sleep
 import sys
 import numpy
 import pylab
-
-# Get required number of points and output filename from commandline
-#npts = 10
-npts = int(sys.argv[1])
-#savename = None
-savename = sys.argv[2]
+import argparse
 
 # Connect to devices
 ITC = MercuryITC('COM3') # PI USB-to-serial connection COM3
-t = ITC.modules[0] # module 0 is temperature board 
+t = ITC.modules[0] # module 0 is temperature board
 #h = ITC.modules[1] # module 1 is heater power board
 
 PSU = TenmaPSU('COM4') # USK-K-R-COM USB-to-serial connection COM4, must be connected via USB hub
 #print(PSU.GetIdentity()) # Prints PSU device identity string to terminal
 
-#National Instruments GPIB-USB-HS GPIB interface
-Vdmm = K2000(16,0) # GPIB adaptor gpib0, device address 16
-Vdmm.write(":SENS:FUNC 'VOLT:DC'") # configure to dc voltage
-Idmm = K2000(26,0) # GPIB adaptor gpib0, device address 26
-Idmm.write(":SENS:FUNC 'CURR:DC'") # configure to dc current
 
-temp = t.tset 
-# read in the temperature setpoint. t.temp returns a tuple containing the latest 
-# temperature reading(float) as element 0 and unit(string) as element 1
-print('Temperature set to {} {}'.format(temp[0],temp[1])) # print to screen
+def get_args():
+    parser = argparse.ArgumentParser(description='Do some measurements.')
+    parser.add_argument('-n', '--npts', type=int, required=True, default=10, help='')
+    parser.add_argument('-s', '--savename', type=str, required=True, default='output.txt',  help='')
+    parser.add_argument('-t', '--temp', type=int, required=True, default=77, help='Init temp')
+    parser.add_argument('-u', '--tempstep', type=int, required=True, default=5, help='Temp step')
 
-PSU.SetCurrent=0.01 #A
-# write the PSU current setpoint (float, in Ampere units)
-print('PSU current output set to {} A'.format(PSU.SetCurrent))
-# read back PSU current setpoint value and print to terminal
+    return parser.parse_args()
 
 
-# initialise data arrays
-T = numpy.zeros(npts)
-V = numpy.zeros(npts)
-I = numpy.zeros(npts)
-#R = numpy.zeros(npts)
+def set_temp(temp):
+    t.tset = temp
+    reading = t.tset
 
-# loop to take repeated readings
-for p in range(npts):
-    #PSU.SetCurrent=0.002*p #A - current ramp
-	#	or
-    #PSU.SetVoltage=0.05*p #V - voltage ramp
+    # read in the temperature setpoint. t.temp returns a tuple containing the latest
+    # temperature reading(float) as element 0 and unit(string) as element 1
+    print('Temperature set to {} {}'.format(reading[0], reading[1]))  # print to screen
 
-    sleep(0.1) # pause before taking readings
-    T[p]=t.temp[0] 
-    # t.temp returns a tuple containing the latest temperature reading (float) 
-    # as element 0 and unit(string) as element 1
-    V[p]=Vdmm.reading # *dmm.reading returns latest reading from *dmm (float, in Volt or Ampere units)
-    I[p]=Idmm.reading
-    #R[p]=V[p]/I[p] # calculate resistance - note the __future__ division import...
-    print(p, T[p], V[p], I[p]) # print to screen
- 
-PSU.OutputOff	# Turn off PSU output
-
-if not (savename==None):      
-    numpy.savetxt(savename, (T,V,I)) # save data to file
-
-#t.tset = 100.0 # set temperture setpoint on ITC to next required value
-temp=t.tset # read in the temperature setpoint
-print('Temperature set to {} {}'.format(temp[0],temp[1])) # print to screen
-
-#Disconnect from instruments
-PSU.close()
-ITC.close()
+    sleep(60 * 15)
 
 
-# plot a graph
-pylab.figure()
-#pylab.plot(T, '-k', marker='x')
-pylab.plot(V, '-b', marker='x')
-#pylab.plot(I, '-r', marker='x')
-#pylab.plot(R, '-r', marker='o')
-pylab.show()
+def set_psu():
+    # National Instruments GPIB-USB-HS GPIB interface
+    Vdmm = K2000(16, 0)  # GPIB adaptor gpib0, device address 16
+    Vdmm.write(":SENS:FUNC 'VOLT:DC'")  # configure to dc voltage
+    Idmm = K2000(26, 0)  # GPIB adaptor gpib0, device address 26
+    Idmm.write(":SENS:FUNC 'CURR:DC'")  # configure to dc current
+
+    PSU.SetCurrent = 0.01 #A
+    # write the PSU current setpoint (float, in Ampere units)
+    print('PSU current output set to {} A'.format(PSU.SetCurrent))
+    # read back PSU current setpoint value and print to terminal
+
+    PSU.SetVoltage = 0.5  # V
+    # write the PSU current setpoint (float, in Ampere units)
+    print('PSU voltage output set to {} A'.format(PSU.SetVoltage))
+    # read back PSU current setpoint value and print to terminal
+
+
+def iterate_temp(npts, temp, tstep, savename):
+    # initialise data arrays
+    T = numpy.zeros(npts)
+    V = numpy.zeros(npts)
+    I = numpy.zeros(npts)
+    R = numpy.zeros(npts)
+
+
+    # loop to take repeated readings
+    for p in range(npts):
+        set_temp(temp)
+
+        T[p] = t.temp[0]
+        # t.temp returns a tuple containing the latest temperature reading (float)
+        # as element 0 and unit(string) as element 1
+        V[p] = Vdmm.reading # *dmm.reading returns latest reading from *dmm (float, in Volt or Ampere units)
+        I[p] = Idmm.reading
+        R[p] = V[p] / I[p] # calculate resistance - note the __future__ division import...
+        print(p, T[p], V[p], I[p]) # print to screen
+
+        # plot a graph
+        pylab.figure()
+        pylab.plot(T, '-k', marker='x')
+        pylab.plot(V, '-b', marker='x')
+        pylab.plot(I, '-r', marker='x')
+        pylab.plot(R, '-r', marker='o')
+        pylab.show()
+
+        temp += tstep
+
+    if not (savename == None):
+        numpy.savetxt(savename, (T, V, I))  # save data to file
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    set_psu()
+
+    iterate_temp(args.npts, args.temp, args.tstep, args.savename)
+
+    PSU.OutputOff  # Turn off PSU output
+
+    # Disconnect from instruments
+    PSU.close()
+    ITC.close()
