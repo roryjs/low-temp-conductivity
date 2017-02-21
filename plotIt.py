724dfcbd48578c0a780cd5f501bfea4d872ecf87
Rory J Sanderson <rory@rjsanderson.com>
2017-02-21 17:34:54 +0000

    Correctly import output txt file

--- plotIt.py
+++ plotIt.py
@@ -1,15 +1,16 @@
 import numpy
-import pylab
+import matplotlib.pyplot as pyplot
+from numpy import array as array
 import os
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-txt = open(os.path.join(__location__, 'output2.txt'));
-(T, V, I, ti) = numpy.loadtxt(txt)
-pylab.figure()
-pylab.plot(T, V/I)
+txt = open(os.path.join(__location__, 'output.txt')).read()
+(T, V, I, ti) = eval(txt)
+
+pyplot.figure()
+pyplot.plot(T, V/I)
 #pylab.plot(ti, V)
 #pylab.plot(ti, I)
 
-pylab.show()
+pyplot. show()
