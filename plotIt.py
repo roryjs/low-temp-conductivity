c81472d41c011ec996bcede08bedfc55b4ee7e0b
Rory J Sanderson <rory@rjsanderson.com>
2017-02-27 15:53:42 +0000

    Temp add code for new read method

--- plotIt.py
+++ plotIt.py
@@ -3,14 +3,22 @@ import matplotlib.pyplot as pyplot
 from numpy import array as array
 import os
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-txt = open(os.path.join(__location__, 'output.txt')).read()
-(T, V, I, ti) = eval(txt)
+(T, V, I, ti) = numpy.loadtxt('output3.txt')
 
 pyplot.figure()
 pyplot.plot(T, V/I)
 #pylab.plot(ti, V)
 #pylab.plot(ti, I)
 
-pyplot. show()
+pyplot.show()
+
+
+# new code to write txt
+
+numpy.savetxt('output3.txt', (T, V, I, ti))
+
+with open('output3.txt') as f:
+    data = f.readlines()
+
+print data
