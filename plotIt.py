c4689a9a790dc67935d0e2460bccce1abc1f5406
Rory J Sanderson <rory@rjsanderson.com>
2017-02-28 16:13:09 +0000

    Update plot it

--- plotIt.py
+++ plotIt.py
@@ -4,10 +4,17 @@ from numpy import array as array
 import os
 
 
-(T, V, I, ti) = numpy.loadtxt('output3.txt')
+(T, V, I, ti, ti_temp, temps) = numpy.loadtxt('output2.txt')
+ti_temp = numpy.trim_zeros(ti_temp)
+temps = numpy.trim_zeros(temps)
+
+#(T, V, I, ti, ti_I, currents) = numpy.loadtxt('output2.txt')
+#ti_I = numpy.trim_zeros(ti_I)
+#currents = numpy.trim_zeros(currents)
 
 pyplot.figure()
-pyplot.plot(T, V/I)
+pyplot.plot(ti, T)
+pyplot.plot(ti, V/I)
 #pylab.plot(ti, V)
 #pylab.plot(ti, I)
 
