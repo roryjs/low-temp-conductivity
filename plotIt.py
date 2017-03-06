11d344ad7cf184b790c0c575661703ce94bb454b
Rory J Sanderson <rory@rjsanderson.com>
2017-03-06 15:37:14 +0000

    Update plot it

--- plotIt.py
+++ plotIt.py
@@ -1,12 +1,11 @@
 import numpy
 import matplotlib.pyplot as pyplot
-from numpy import array as array
-import os
 
-
-(T, V, I, ti, ti_temp, temps) = numpy.loadtxt('output2.txt')
-ti_temp = numpy.trim_zeros(ti_temp)
-temps = numpy.trim_zeros(temps)
+(T, V, I, ti, ti_temp, temps) = numpy.loadtxt('semi-80to140-5step.txt')
+ti_temp = numpy.trim_zeros(ti_temp, 'b')
+temps = numpy.trim_zeros(temps, 'b')
+print temps
+print ti_temp
 
 #(T, V, I, ti, ti_I, currents) = numpy.loadtxt('output2.txt')
 #ti_I = numpy.trim_zeros(ti_I)
@@ -14,7 +13,7 @@ temps = numpy.trim_zeros(temps)
 
 pyplot.figure()
 pyplot.plot(ti, T)
-pyplot.plot(ti, V/I)
+#pyplot.plot(ti, V/I)
 #pylab.plot(ti, V)
 #pylab.plot(ti, I)
 
